2e39f5c95b40c57554bb078b5f4570ee0cf6a197
Andreas Leicher <email@andreasleicher.com>
2018-08-29 10:23:23 +0200

    refactor(air): use version 18.3.1, DRY the start scripot

--- TODO.md
+++ TODO.md
@@ -1,3 +1,4 @@
+- fix the way we start the air, do not use a secret
 - snippets contains the commands to create the secrets, make that a bash script
 - start_the_air.sh shell script is used to bring up the air, permission topic, we'll find a better solution
   - can we run the container as deployer:deployer
--- k8s/start_the_air.sh
+++ k8s/start_the_air.sh
@@ -7,26 +7,27 @@ function log {
   echo "[aircloak] $msg"
 }
 
-BASE_FOLDER=/aircloak/air/lib/air-18.3.0/priv
+VERSION=air-18.3.1
+BASE_FOLDER=/aircloak/air/lib/${VERSION}/priv
 CONFIG_FOLDER=${BASE_FOLDER}/config
 
 log "Booting container."
-mkdir -p /aircloak/air/lib/air-18.3.0/priv/config/
+mkdir -p ${CONFIG_FOLDER}/
 log "ls priv/"
-ls -l /aircloak/air/lib/air-18.3.0/priv/
+ls -l ${BASE_FOLDER}/
 log "ls /runtime_config"
 ls -l /runtime_config
 #log "copying..."
-#cp -rp /runtime_config/* /aircloak/air/lib/air-18.3.0/priv/config/
+#cp -rp /runtime_config/* ${CONFIG_FOLDER}/
 
 log "cat to the rescue"
-cat /runtime_config/config.json > /aircloak/air/lib/air-18.3.0/priv/config/config.json
+cat /runtime_config/config.json > ${CONFIG_FOLDER}/config.json
 
 log "copied the config, ls /priv/config"
-ls -l /aircloak/air/lib/air-18.3.0/priv/config/
+ls -l ${CONFIG_FOLDER}/
 log "chmodding"
-chown deployer:deployer -R /aircloak/air/lib/air-18.3.0/priv/config/
+chown deployer:deployer -R ${CONFIG_FOLDER}/
 log "after chmodding, ls /priv/config"
-ls -l /aircloak/air/lib/air-18.3.0/priv/config/
+ls -l ${CONFIG_FOLDER}/
 log "now booting..."
 exec gosu deployer /aircloak/air/bin/air foreground
